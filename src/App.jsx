0a3d56b62533b8287046842469f3d9ce31f7af29
Rea <rhasanaj@yahoo.com>
2024-05-15 23:39:42 +0530

    Created Post component.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Post from "./components/Post"
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Fakebook!</h1>
+      {/* CreatePostForm */}
+      {/* Feed*/}
+      <Post/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+function Post(){
+    return(
+        <div>
+            <h3>Test Post</h3>
+            <p>This is a test post we'll edit later!</p>
+            <p>Likes: 30</p>
+            <p>Comments: </p>
+        </div>
+    )
+}
+
+export default Post
